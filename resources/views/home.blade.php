075c743d7ebe8a14ebf7a7577d2d8da3747efb05
Marco Ranieri <marcoranieri0451@gmail.com>
2021-07-04 16:54:47 +0200

    base layout + bootstrap import

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-12 text-center">
+
+                <h1>Welcome</h1>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sail\Console\PublishCommand;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PublicController::class, 'home'])->name('home');
