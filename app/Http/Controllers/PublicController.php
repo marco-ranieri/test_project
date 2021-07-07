2d51cb3bcdeafbc3c7c12566cc4ab2fd45ca3000
Marco Ranieri <marcoranieri0451@gmail.com>
2021-07-07 17:44:12 +0200

    last announcements in homepage + oneToMany User - Announcements

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
     public function home() {
-            return view('home');
-        }
+
+        $announcements = Announcement::orderBy('created_at', 'DESC')->take(5)->get();
+        return view('home', compact('announcements'));
+
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,6 +29,29 @@
                 <h1>Welcome</h1>
             </div>
         </div>
+
+        @foreach ($announcements as $announcement)
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-8 py-3">
+                    <div class="card">
+                        <div class="card-header"> {{$announcement->title}}</div>
+
+                        <div class="card-body">
+                            <p>
+                                <img src="https://via.placeholder.com/300x150.png" class="rounded float-right"alt="">
+                                {{$announcement->body}}
+                            </p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between">
+                            Category: <a href="">{{$announcement->category->name}}</a>
+                            <i>Annuncio creato il {{$announcement->created_at->format('d/m/Y')}}</i>
+                            <i>ID annuncio: {{$announcement->id}}</i>
+                            <i>Inserito da: {{$announcement->user->name}}</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 
 </x-layout>
